5de9d09d6ee7456c8effbd577ea3ba1085e4df02
Hayoon Park <the.day0619@gmail.com>
2023-03-01 21:29:30 +0900

    ready for IM

--- SWEA/SW Apply/SEA_1860_진기의 최고급 붕어빵.py
+++ SWEA/SW Apply/SEA_1860_진기의 최고급 붕어빵.py
@@ -1,10 +1,19 @@
+'''
+0. 손님이 오기전까지 구워진 붕어빵을 계산하고, 앞에 손님이 다녀간 만큼 빼준 것이 현재 붕어빵 수!
+1. 손님이 오는 시간은 제각각 다르므로 sort 메서드로 오름차순 정리 해준다.
+2. 손님이 왔을때 붕어빵의 수가 0 이거나 음수면 Impossible 출력!
+'''
+
 for tc in range(1, int(input())+1):
     N, M, K = map(int, input().split())
-    cus = [list(map(int, input().split())) for _ in range(N)]
-    cus.sort()
-    brd = [0] * (max(cus)+1)
-    brd[M] = K
-    if cus[0] < M:
-        ans = 'Impossible'
-    else:
-        for i in range(len(cus)):
+    cus = list(map(int, input().split()))
+    cus.sort()    # 오는 시간을 오름차순으로 정리
+    ans = 'Possible'
+    rslt = 0
+    for n in range(N):
+        rslt = (cus[n] // M)*K - n
+        if rslt <= 0:
+            ans = 'Impossible'
+            break
+
+    print(f'#{tc}', ans)
