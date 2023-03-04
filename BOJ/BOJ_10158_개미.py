590be960feecd13cf86dc031dbfb40b68f37e1d8
hyoonpark <the.day0619@gmail.com>
2023-03-04 22:16:36 +0900

    ready for IM

--- BOJ/BOJ_10158_개미.py
+++ BOJ/BOJ_10158_개미.py
@@ -1,3 +1,18 @@
 w, h = map(int, input().split())
 p, q = map(int, input().split())
 t = int(input())
+
+j = (p + t) // w
+i = (q + t) // h
+
+if j % 2 == 0:
+    x = (p + t) % w
+else:
+    x = w - (p + t) % w
+
+if i % 2 == 0:
+    y = (q + t) % h
+else:
+    y = h - (q + t) % h
+
+print(x, y)
