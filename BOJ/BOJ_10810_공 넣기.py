0fc20e6bb41b82ece65bcead8588ac504efeebe1
hyoonpark <the.day0619@gmail.com>
2023-03-07 22:46:01 +0900

    personal study

---
+++ BOJ/BOJ_10810_공 넣기.py
@@ -0,0 +1,7 @@
+N, M = map(int, input().split())
+basket = [0] * (N+1)
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    for a in range(i, j+1):
+        basket[a] = k
+print(*basket[1:])
